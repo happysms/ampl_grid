1584abb9521673d8eed7effff26bd366afedbcd4
minsung <khuminsung@khu.ac.kr>
2022-11-04 16:24:15 +0900

    new sell buy price func

--- gridBot.js
+++ gridBot.js
@@ -1,6 +1,7 @@
 let config = require('./config');
 let ccxt = require('ccxt');
 
+
 let getGridList = (lowerPrice, upperPrice, middlePrice, gridNum) => {
     let gridList = [];
     let halfGridNum = gridNum / 2;
@@ -8,20 +9,33 @@ let getGridList = (lowerPrice, upperPrice, middlePrice, gridNum) => {
     for (let i = 1; i < halfGridNum + 1; i++) {
         let gridPrice = middlePrice - i * (middlePrice - lowerPrice) / halfGridNum;
         gridPrice = parseFloat(gridPrice.toFixed(4));
-        let gridSize = parseInt(((1 / gridPrice) * 100) / 10 + 1) * 10
+        let gridSize = parseInt(((1 / gridPrice) * 200) / 10 + 1) * 10
         gridList.push([gridPrice, gridSize]);
     }
 
-    gridList.push([middlePrice, parseInt(((1 / middlePrice) * 100) / 10 + 1) * 10]);
+    gridList.push([middlePrice, parseInt(((1 / middlePrice) * 200) / 10 + 1) * 10]);
 
     for (let i = 1; i < halfGridNum + 1; i++) {
         let gridPrice = middlePrice + i * (upperPrice - middlePrice) / halfGridNum;
         gridPrice = parseFloat(gridPrice.toFixed(4));
-        let gridSize = parseInt(((1 / gridPrice) * 100) / 10 + 1) * 10;
+        let gridSize = parseInt(((1 / gridPrice) * 200) / 10 + 1) * 10;
         gridList.push([gridPrice, gridSize]);
     }
     return gridList;
 }
+
+
+let getOrderSize = (price) => {
+    if (price > config.MIDDLE_PRICE) {
+        var gridSize = parseInt(((1 / price) * 200) / 10 + 1) * 10
+    } else if (price < config.MIDDLE_PRICE) {
+        var gridSize = parseInt(((1 / price) * 200) / 10 + 1) * 10
+    } else {
+        var gridSize = parseInt(((1 / config.MIDDLE_PRICE) * 200) / 10 + 1) * 10
+    }
+    return gridSize;
+}
+
 
 
 (async function () {
@@ -66,8 +80,9 @@ let getGridList = (lowerPrice, upperPrice, middlePrice, gridNum) => {
                 closedOrderIds.push(orderInfo['id']);
                 console.log(`buy order executed at ${orderInfo['price']}`);
                 let newSellPrice = parseFloat(orderInfo['price'] + config.GRID_SIZE);
+                let size = getOrderSize(newSellPrice);
                 console.log(`creating new limit sell order at ${newSellPrice}`);
-                let newSellOrder = await exchange.createLimitSellOrder(config.SYMBOL, config.POSITION_SIZE, newSellPrice);
+                let newSellOrder = await exchange.createLimitSellOrder(config.SYMBOL, size, newSellPrice);
                 sellOrders.push(newSellOrder);
             }
 
@@ -88,8 +103,9 @@ let getGridList = (lowerPrice, upperPrice, middlePrice, gridNum) => {
                 closedOrderIds.push(orderInfo['id']);
                 console.log(`sell order executed at ${orderInfo['price']}`);
                 let newBuyPrice = parseFloat(orderInfo['price']) - config.GRID_SIZE;
+                let size = getOrderSize(newBuyPrice);
                 console.log(`creating new limit buy order at ${newBuyPrice}`);
-                let newBuyOrder = await exchange.createLimitBuyOrder(config.SYMBOL, config.POSITION_SIZE, newBuyPrice);
+                let newBuyOrder = await exchange.createLimitBuyOrder(config.SYMBOL, size, newBuyPrice);
                 buyOrders.push(newBuyOrder);
             }
             await new Promise(resolve => setTimeout(resolve, config.CHECK_ORDERS_FREQUENCY));
